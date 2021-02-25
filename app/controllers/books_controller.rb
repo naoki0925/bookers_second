113ec9fe5e167c7ca83cf4483dfb0a4087acf3c2
naoki0925 <naoking250912@icloud.com>
2021-02-25 15:01:55 +0000

    topとindex

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,31 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new (book_params)
+    @book.save
+    redirect_to book_path (book)
+  end
+
+  def show
+    @book = Book.new
+    @books = Book.find(params[:id])
+  end
+
+  def edit
+    @books = Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body,)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_024802) do
+ActiveRecord::Schema.define(version: 2021_02_25_133953) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
