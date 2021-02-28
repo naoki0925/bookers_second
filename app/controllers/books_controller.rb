bfb75e0973ef528478c0874fb41a1cb020b921e5
naoki0925 <naoking250912@icloud.com>
2021-02-28 08:10:40 +0000

    save

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new (book_params)
     @book.save
-    redirect_to book_path (book)
+    redirect_to book_path (@book)
   end
 
   def show
@@ -19,7 +19,7 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @books = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,10 @@
 
 ActiveRecord::Schema.define(version: 2021_02_25_133953) do
 
-  create_table "blogs", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
